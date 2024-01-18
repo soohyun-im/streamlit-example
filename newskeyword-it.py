72eef61d981e3500096f51e06896760800182949
soohyun-im <156729117+soohyun-im@users.noreply.github.com>
2024-01-18 13:54:04 +0900

    Update newskeyword-it.py

--- newskeyword-it.py
+++ newskeyword-it.py
@@ -2,7 +2,7 @@ import streamlit as st
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
-import openpyxl
+import openpyxl from pandas
 from retrying import retry
 import os
 import openai
